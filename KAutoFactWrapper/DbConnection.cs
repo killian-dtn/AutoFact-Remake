4f8ab4b3641c65b894c45e4f9af7fd8d72a14f13
killian-dtn <killian.dantansentr@gmail.com>
2020-04-22 01:49:20 +0200

    Init Singleton

--- KAutoFactWrapper/DbConnection.cs
+++ KAutoFactWrapper/DbConnection.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace KAutoFactWrapper
 {
     public class DbConnection
     {
-    }
+        private Wrapper Wp;
+		private string ConnectionString;
+
+		private static DbConnection instance = null;
+		public static DbConnection Instance
+		{
+			get
+			{
+				if (DbConnection.instance == null)
+					DbConnection.instance = new DbConnection();
+				return DbConnection.instance;
+			}
+		}
+
+		public MySqlConnection Connection { get; private set; }
+
+		private DbConnection()
+		{
+			this.Wp = Wrapper.Instance;
+			this.Connection = new MySqlConnection();
+		}
+	}
 }
