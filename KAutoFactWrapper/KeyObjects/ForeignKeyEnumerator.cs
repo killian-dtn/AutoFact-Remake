99716aacb85b79f953153b9ab94e031ee2d11d39
killian-dtn <killian.dantansentr@gmail.com>
2020-04-23 21:05:40 +0200

    Utilisation de Dictionary<PropertyInfo, PropertyInfo> au lieu de ForeignKey[]

--- KAutoFactWrapper/KeyObjects/ForeignKeyEnumerator.cs
+++ KAutoFactWrapper/KeyObjects/ForeignKeyEnumerator.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +10,12 @@ namespace KAutoFactWrapper
 {
     class ForeignKeyEnumerator : IEnumerator
     {
-        public ForeignKey[] ForeignKeys { get; private set; }
-        public ForeignKey Current
+        public Dictionary<PropertyInfo, PropertyInfo> ForeignKeys { get; private set; }
+        public KeyValuePair<PropertyInfo, PropertyInfo> Current
         {
             get
             {
-                try { return this.ForeignKeys[this.Position]; }
+                try { return this.ForeignKeys.ElementAt(this.Position)/*[this.Position]*/; }
                 catch(IndexOutOfRangeException) { throw new InvalidOperationException(); }
             }
         }
@@ -27,7 +28,7 @@ namespace KAutoFactWrapper
         }
         public int Position { get; private set; }
 
-        public ForeignKeyEnumerator(ForeignKey[] foreignKeys)
+        public ForeignKeyEnumerator(Dictionary<PropertyInfo, PropertyInfo> foreignKeys)
         {
             this.ForeignKeys = foreignKeys;
             this.Position = -1;
@@ -36,7 +37,7 @@ namespace KAutoFactWrapper
         public bool MoveNext()
         {
             this.Position++;
-            return (this.Position < this.ForeignKeys.Length);
+            return (this.Position < this.ForeignKeys.Count);
         }
 
         public void Reset()
--- KAutoFactWrapper/KeyObjects/ForeignKeyStruct.cs
+++ KAutoFactWrapper/KeyObjects/ForeignKeyStruct.cs
@@ -2,19 +2,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using KAutoFactWrapper.Exceptions;
 
 namespace KAutoFactWrapper
 {
     class ForeignKeyStruct : IEnumerable
     {
-        public ForeignKey[] ForeignKeys { get; private set; }
+        public Dictionary<PropertyInfo, PropertyInfo> ForeignKeys { get; private set; }
+        public PropertyInfo this[PropertyInfo Item]
+        {
+            get
+            {
+                try { return this.ForeignKeys[Item]; }
+                catch(IndexOutOfRangeException e) { throw new DbAttributeException($"{Item.ReflectedType.FullName}.{Item.Name} n'est pas répertoriée comme clé étrangère.", e); }
+            }
+        }
 
-        public ForeignKeyStruct(ForeignKey[] foreignKeys)
+        public ForeignKeyStruct(Dictionary<PropertyInfo, PropertyInfo> foreignKeys)
         {
             this.ForeignKeys = foreignKeys;
         }
+
+        #region IEnumerable
 
         IEnumerator IEnumerable.GetEnumerator()
         {
@@ -25,5 +37,7 @@ namespace KAutoFactWrapper
         {
             return new ForeignKeyEnumerator(this.ForeignKeys);
         }
+
+        #endregion
     }
 }
