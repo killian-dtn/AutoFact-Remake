389ac8bbac75fcb180cc25b3da56dc3a773ad2c2
killian-dtn <killian.dantansentr@gmail.com>
2020-04-24 01:21:04 +0200

    Division DbPropAttribute

---
+++ KAutoFactWrapper/Attributes/DbPropAttribute/DbForeignKeyPropAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAutoFactWrapper.Attributes
+{
+    public class DbForeignKeyPropAttribute : DbPropAttribute, IForeignKeyPropAttribute
+    {
+        public string ReferenceDbName { get; private set; }
+        public string ReferenceTable { get; private set; }
+        public RelationType Relation { get; private set; }
+        string IForeignKeyPropAttribute.ReferenceDbName { get { return this.ReferenceDbName; } set { this.ReferenceDbName = value; } }
+        string IForeignKeyPropAttribute.ReferenceTable { get { return this.ReferenceTable; } set { this.ReferenceTable = value; } }
+        RelationType IForeignKeyPropAttribute.Relation { get { return this.Relation; } set { this.Relation = value; } }
+
+        private DbForeignKeyPropAttribute(string dbName, string referenceDbName, string referenceTable, RelationType relation) : base(dbName)
+        {
+            this.ReferenceDbName = referenceDbName;
+            this.ReferenceTable = referenceTable;
+            this.Relation = relation;
+        }
+
+        public DbForeignKeyPropAttribute(string dbName, string referenceDbName, string referenceTable) : this(dbName, referenceDbName, referenceTable, RelationType.none) { }
+    }
+}
---
+++ KAutoFactWrapper/Attributes/DbPropAttribute/DbPrimaryKeyPropAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAutoFactWrapper.Attributes
+{
+    public class DbPrimaryKeyPropAttribute : DbPropAttribute, IPrimaryKeyPropAttribute
+    {
+        public DbPrimaryKeyPropAttribute(string dbName) : base(dbName) { }
+    }
+}
