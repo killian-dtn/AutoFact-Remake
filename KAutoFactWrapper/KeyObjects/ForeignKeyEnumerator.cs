4c63d4a345635dea7595422bf4171275e049b7f6
killian-dtn <killian.dantansentr@gmail.com>
2020-04-23 18:51:51 +0200

    Ajout ForeignKeyStruct & ForeignKeyEnumerator

---
+++ KAutoFactWrapper/KeyObjects/ForeignKeyEnumerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAutoFactWrapper
+{
+    class ForeignKeyEnumerator : IEnumerator
+    {
+        public ForeignKey[] ForeignKeys { get; private set; }
+        public ForeignKey Current
+        {
+            get
+            {
+                try { return this.ForeignKeys[this.Position]; }
+                catch(IndexOutOfRangeException) { throw new InvalidOperationException(); }
+            }
+        }
+        object IEnumerator.Current
+        {
+            get
+            {
+                return this.Current;
+            }
+        }
+        public int Position { get; private set; }
+
+        public ForeignKeyEnumerator(ForeignKey[] foreignKeys)
+        {
+            this.ForeignKeys = foreignKeys;
+            this.Position = -1;
+        }
+
+        public bool MoveNext()
+        {
+            this.Position++;
+            return (this.Position < this.ForeignKeys.Length);
+        }
+
+        public void Reset()
+        {
+            this.Position = -1;
+        }
+    }
+}
---
+++ KAutoFactWrapper/KeyObjects/ForeignKeyStruct.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KAutoFactWrapper
+{
+    class ForeignKeyStruct : IEnumerable
+    {
+        public ForeignKey[] ForeignKeys { get; private set; }
+
+        public ForeignKeyStruct(ForeignKey[] foreignKeys)
+        {
+            this.ForeignKeys = foreignKeys;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator)this.GetEnumerator();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return new ForeignKeyEnumerator(this.ForeignKeys);
+        }
+    }
+}
