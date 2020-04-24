f851327691de05272b003bc2a9783c1771247f7b
killian-dtn <killian.dantansentr@gmail.com>
2020-04-25 01:08:48 +0200

    Ajout classes de test

---
+++ TestsKAutoFactWrapper/TestClasses/Baz.cs
@@ -0,0 +1,18 @@
+﻿using KAutoFactWrapper.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsKAutoFactWrapper.TestClasses
+{
+    [DbClass("BAZ")]
+    class Baz : Bar
+    {
+        [DbPrimaryForeignKeyProp("ID", "ID", "BAR")]
+        public new int Id { get; private set; }
+        [DbProp("BAZ_ITEM")]
+        public int BazItem { get; private set; }
+    }
+}
---
+++ TestsKAutoFactWrapper/TestClasses/Foo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KAutoFactWrapper;
+using KAutoFactWrapper.Attributes;
+
+namespace TestsKAutoFactWrapper.TestClasses
+{
+    [DbClass("FOO")]
+    class Foo : BaseEntity
+    {
+        [DbPrimaryKeyProp("ID")]
+        public int Id { get; private set; }
+        [DbProp("FOO_ITEM")]
+        public int FooItem { get; private set; }
+    }
+}
